e1b5eb2e07333a45d0e87a1ac0e36c8f4ae5bc89
Marco Zollo <zollomarco23@gmail.com>
2026-07-06 21:25:14 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<h1>Home</h1>} />
+          <Route path='/movies' element={<h1>Movies</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
